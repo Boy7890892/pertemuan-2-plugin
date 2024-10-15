29f87e1aaba90e42f06da890112caa6497119688
Boy7890892 <akhmadridwan357@gmail.com>
2024-10-15 19:22:46 +0700

    pertemuan2

---
+++ dist/function.js
@@ -0,0 +1,6 @@
+"use strict";
+// Function Declaration 
+function sapa(nama, umur) {
+    console.log("Halo, aku " + nama + "umurku" + umur);
+}
+sapa("yasir", 20);
---
+++ src/function.ts
@@ -0,0 +1,5 @@
+// Function Declaration 
+function sapa(nama : string,umur : number) {
+    console.log("Halo, aku " + nama + "umurku" + umur);
+}
+sapa("yasir", 20)
